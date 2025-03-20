e0bb4c1a25202a344d4451ad6acd192387058ecd
Ngan Nguyen Minh <ngan.nguyen3@citek.vn>
2025-03-20 18:06:49 +0000

    firstdeploy

---
+++ citek.js
@@ -0,0 +1,35 @@
+
+/*!
+ *  CanvasInput v1.2.7
+ *  http://goldfirestudios.com/blog/108/CanvasInput-HTML5-Canvas-Text-Input
+ *
+ *  (c) 2013-2017, James Simpson of GoldFire Studios
+ *  goldfirestudios.com
+ *
+ *  MIT License
+ */
+
+(function() {
+    // create a buffer that stores all inputs so that tabbing
+    // between them is made possible.
+    var inputs = [];
+  
+    // initialize the Canvas Input
+    var Citek = window.Citek = function(o) {
+      var self = this;
+     
+   
+    };
+  
+    // setup the prototype
+    Citek.prototype = {
+      /**
+       * Get/set the main canvas.
+       * @param  {Object} data Canvas reference.
+       * @return {Mixed}      CanvasInput or current canvas.
+       */
+      oninit: function(data) {
+        console.log('Citek')
+      }
+    };
+  })();
